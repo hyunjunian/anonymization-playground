fc3954892dafcf8799276bb599c7b4d688f70f64
hyunjunian <hyunjunian@gmail.com>
2025-08-07 15:05:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Example from './Example';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-dvh">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
